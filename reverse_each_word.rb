a9a0348dd20eb0bf54487088a403e4fb817275d9
Xinran Che <gracexinran@gmail.com>
2019-05-19 22:24:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(string)
   array = string.split
   result = []
   array.each do |i|
-    result.push(i.reverse) 
+    result.push(i.reverse)
   end
   puts "#{result.join(' ')}"
+end
